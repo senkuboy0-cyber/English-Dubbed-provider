8716e557d5221faec507b80fc0b5353f3e7a7d7e
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-06-19 21:56:30 +0600

    Add Anichi extension - parser and build config (no local.properties required)

---
+++ Anichi/build.gradle.kts
@@ -0,0 +1,27 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    // description = "Lorem Ipsum"
+     authors = listOf("senkuboy0-cyber")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "AnimeMovie",
+        "Anime",
+        "OVA",
+    )
+    iconUrl = "https://raw.githubusercontent.com/phisher98/TVVVV/refs/heads/main/Icons/Allanime.png"
+
+    isCrossPlatform = true
+}
