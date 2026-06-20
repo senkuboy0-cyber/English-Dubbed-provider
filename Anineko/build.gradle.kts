e0e0b0f4ddd1ab74c9d6c358278b0e6586719f88
senkuboy0-cyber <255331092+senkuboy0-cyber@users.noreply.github.com>
2026-06-20 10:33:44 +0600

    Add Anineko extension - https://anineko.to HTML-based anime streaming (3 categories, 5 extractors)

---
+++ Anineko/build.gradle.kts
@@ -0,0 +1,23 @@
+// use an integer for version numbers
+version = 1
+
+
+cloudstream {
+    // All of these properties are optional, you can safely remove them
+
+    description = "Anime from Anineko"
+    authors = listOf("senkuboy0-cyber")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1
+    tvTypes = listOf("Anime", "AnimeMovie", "OVA")
+    language = "en"
+    iconUrl = "https://raw.githubusercontent.com/phisher98/TVVVV/refs/heads/main/Icons/Anineko.png"
+    isCrossPlatform = true
+}
